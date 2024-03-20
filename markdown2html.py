af04a8d59a5e8e1845f636e10a84bc7af006ead9
jassyio <joejassyio@gmail.com>
2024-03-20 12:12:09 +0300

    markdown2html

---
+++ markdown2html.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"""
+Markdown 2 HTML Converter
+Task 1: convert heading
+"""
+# Task 0-Start a script
+# This script takes two arguments: first is a markdown file name and the
+# second is an html output filename
+if __name__ == '__main__':
+
+    import sys
+    import os.path
+
+    def markdown2html():
+        '''markdown2html - converts markdown to html'''
+    if len(sys.argv) < 3:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        sys.exit(1)
+
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        sys.exit(1)
+
+sys.exit(0)
